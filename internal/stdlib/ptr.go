c4839eacd6a59b87308861a89f1394e75c8cddce
hbc <bahe@microsoft.com>
2024-03-10 23:47:39 -0700

    feat: add helper ValOrZero

--- internal/stdlib/ptr.go
+++ internal/stdlib/ptr.go
@@ -4,3 +4,12 @@ package stdlib
 func Ptr[T any](v T) *T {
 	return &v
 }
+
+// ValOrZero returns the value of v if it's not nil, and a zero value otherwise.
+func ValOrZero[T any](v *T) T {
+	if v == nil {
+		var zero T
+		return zero
+	}
+	return *v
+}
